f4cad1b7a7c0b8c901916db97fc405442bdecf37
Sumitsaman <94039583+Sumitsaman@users.noreply.github.com>
2024-02-19 19:37:54 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+/*int main()
+a=5,b=5
+if(a>b)
+{
+
+
+print("A is sabse bada ")
+
+}
+else if(b>a)
+{
+    print("B is sabse bada ")
+}
+else
+{
+    print("BOTH equal ")
+}
+
+
+int main()
+
+a=6;
+
+{
+
+printf("%d,%d",a++,++a);
+
+return 0;
+}*/
+
+
+
+int main()
+{
+	   printf("%d,%d\n",10+4,10-4);
+       printf("%f,%f",4.0+2.0,4.0-2.0);
+
+    return 0;
+}
+
+
